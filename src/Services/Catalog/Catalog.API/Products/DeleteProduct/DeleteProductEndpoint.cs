6896a4473269180bad5d1ebc71cce2d0b528778c
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2025-05-30 15:27:49 +0600

    GetProductByCategory

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public class DeleteProductEndpoint
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("DeleteProducts")
+                .Produces<GetProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Delete products Id")
+                .WithDescription("Get all products from the database");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    public class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQuery> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling GetProductByCategoryQuery handle query {@Query}", query);
+
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category)).ToListAsync(cancellationToken);
+
+            return products.Count == 0
+                ? throw new ProductNotFoundException($"No products found in category {query.Category}")
+                : new GetProductByCategoryResult(products);
+        }
+    }
+}
