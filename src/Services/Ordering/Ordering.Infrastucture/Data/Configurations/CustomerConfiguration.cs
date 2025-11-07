a8aa35cbc2dc3129736d6e529126c34764728cb5
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2025-11-07 12:09:10 +0600

    configuration file added for order infrastructure entities

---
+++ src/Services/Ordering/Ordering.Infrastucture/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastucture.Data.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).HasConversion(
+                customerId => customerId.Value,
+                dbId => CustomerId.Of(dbId));
+
+            builder.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.Email).HasMaxLength(100);
+            builder.HasIndex(e => e.Email).IsUnique();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastucture/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastucture.Data.Configurations
+{
+
+
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+
+        }
+    }
+}
