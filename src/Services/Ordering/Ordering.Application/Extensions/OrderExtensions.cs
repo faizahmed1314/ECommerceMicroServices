025bd9878caec575e79b05b93604f64814f537bc
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2025-11-14 18:54:25 +0600

    create extension method for mapping order to order dto

---
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace Ordering.Application.Extensions
+{
+    public static class OrderExtensions
+    {
+        public static IEnumerable<OrderDto> ToOrderDtoList(this IEnumerable<Order> orders)
+        {
+            return orders.Select(order => new OrderDto(
+                order.Id.Value,
+                order.CustomerId.Value,
+                order.OrderName.Value,
+                new AddressDto(
+                    order.ShippingAddress.FirstName,
+                    order.ShippingAddress.LastName,
+                    order.ShippingAddress.EmailAddress,
+                    order.ShippingAddress.AddressLine,
+                    order.ShippingAddress.Street,
+                    order.ShippingAddress.PostalCode,
+                    order.ShippingAddress.City,
+                    order.ShippingAddress.State,
+                    order.ShippingAddress.Country
+                ),
+                new AddressDto(
+                    order.BillingAddress.FirstName,
+                    order.BillingAddress.LastName,
+                    order.BillingAddress.EmailAddress,
+                    order.BillingAddress.AddressLine,
+                    order.BillingAddress.Street,
+                    order.BillingAddress.PostalCode,
+                    order.BillingAddress.City,
+                    order.BillingAddress.State,
+                    order.BillingAddress.Country
+                ),
+                new PaymentDto(
+                    order.Payment.PaymentMethod,
+                    order.Payment.CardNumber,
+                    order.Payment.CardHolderName,
+                    order.Payment.CVV,
+                    order.Payment.Expiration
+                ),
+                order.Status,
+                order.OrderItems.Select(oi => new OrderItemDto(
+                    oi.OrderId.Value,
+                    oi.ProductId.Value,
+                    oi.Price,
+                    oi.Quantity
+                )).ToList()
+            ));
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Orders/Quries/GetOrderByName/GetOrderByNamehandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Quries/GetOrderByName/GetOrderByNamehandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Extensions;
 
 namespace Ordering.Application.Orders.Quries.GetOrderByName
 {
@@ -12,59 +13,7 @@ namespace Ordering.Application.Orders.Quries.GetOrderByName
                 .OrderBy(o => o.OrderName.Value)
                 .ToListAsync(cancellationToken);
 
-            var result = ProjectToOrderList(orderlist);
-            return new GetOrderByNameResult(result);
-        }
-
-        private IEnumerable<OrderDto> ProjectToOrderList(List<Order> orderlist)
-        {
-            List<OrderDto> orders = new();
-            foreach (var order in orderlist)
-            {
-                var orderDto = new OrderDto(
-                    order.Id.Value,
-                    order.CustomerId.Value,
-                    order.OrderName.Value,
-                    new AddressDto(
-                        order.ShippingAddress.FirstName,
-                        order.ShippingAddress.LastName,
-                        order.ShippingAddress.EmailAddress,
-                        order.ShippingAddress.AddressLine,
-                        order.ShippingAddress.Street,
-                        order.ShippingAddress.PostalCode,
-                        order.ShippingAddress.City,
-                        order.ShippingAddress.State,
-                        order.ShippingAddress.Country
-                    ),
-                    new AddressDto(
-                        order.BillingAddress.FirstName,
-                        order.BillingAddress.LastName,
-                        order.BillingAddress.EmailAddress,
-                        order.BillingAddress.AddressLine,
-                        order.BillingAddress.Street,
-                        order.BillingAddress.PostalCode,
-                        order.BillingAddress.City,
-                        order.BillingAddress.State,
-                        order.BillingAddress.Country
-                    ),
-                    new PaymentDto(
-                        order.Payment.PaymentMethod,
-                        order.Payment.CardNumber,
-                        order.Payment.CardHolderName,
-                        order.Payment.CVV,
-                        order.Payment.Expiration
-                    ),
-                    order.Status,
-                    order.OrderItems.Select(oi => new OrderItemDto(
-                        oi.OrderId.Value,
-                        oi.ProductId.Value,
-                        oi.Price,
-                        oi.Quantity
-                    )).ToList()
-                );
-                orders.Add(orderDto);
-            }
-            return orders;
+            return new GetOrderByNameResult(orderlist.ToOrderDtoList());
         }
     }
 }
