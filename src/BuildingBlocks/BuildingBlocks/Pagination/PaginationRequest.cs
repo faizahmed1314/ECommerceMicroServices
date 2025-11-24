745d12dc32203c99a9ef36e385db3a7dddc5a6d6
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2025-11-24 17:57:35 +0600

    create pagination classess at building blocks

---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginationRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace BuildingBlocks.Pagination
+{
+    record class PaginationRequest(int PageNumber = 1, int PageSize = 10);
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.Pagination
+{
+    public class PaginationResult<TEntity>(int pageNumber, int pageSize, int totalRecords, IEnumerable<TEntity> records) where TEntity : class
+    {
+        public int PageNumber { get; } = pageNumber;
+        public int PageSize { get; } = pageSize;
+        public int TotalRecords { get; } = totalRecords;
+        public IEnumerable<TEntity> Records { get; } = records;
+    }
+}
