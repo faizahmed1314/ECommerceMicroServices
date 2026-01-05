d4c75a011682231b237976d9e9a7066f822885c7
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2026-01-05 17:03:39 +0600

    put endpoint created for order

--- src/Services/Ordering/Ordering.API/Endpoint/UpdateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoint/UpdateOrder.cs
@@ -1,10 +1,30 @@
-﻿namespace Ordering.API.Endpoint
+﻿using Ordering.Application.Orders.Commands.UpdateOrder;
+
+namespace Ordering.API.Endpoint
 {
     // accept a updater order request payload
     // map the request to an update order command
     // use the mediator to send the command to the appropriate handler
     // return a response indicating the result of the update operation
-    public class UpdateOrder
+
+    public record UpdateOrderRequest(OrderDto Order);
+    public record UpdateOrderResponse(bool IsSuccess);
+    public class UpdateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/orders", async (UpdateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateOrderCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateOrderResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("UpdateOrder")
+                .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Updates an existing order")
+                .WithDescription("Updates an existing order with the provided details.");
+        }
     }
 }
