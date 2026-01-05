4eda8ed1be64654a0f78351f87368501c5596357
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2026-01-05 17:15:51 +0600

    created get order api

--- src/Services/Ordering/Ordering.API/Endpoint/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoint/GetOrders.cs
@@ -1,10 +1,31 @@
-﻿namespace Ordering.API.Endpoint
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Quries.GetOrders;
+
+namespace Ordering.API.Endpoint
 {
     // accept pagination and filtering parameters
     // construct a GetOrderQuery
     // use the mediator to send the query to the appropriate handler
     // return a response containing the list of orders
-    public class GetOrders
+
+    //public record GetOrdersRequest(PaginationRequest PaginationRequest);
+    public record GetOrdersResponse(PaginationResult<OrderDto> Orders);
+    public class GetOrders : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders", async ([AsParameters] PaginationRequest paginationRequest, ISender sender) =>
+            {
+                var query = new GetOrdersQuery(paginationRequest);
+                var result = await sender.Send(query);
+                var response = result.Adapt<GetOrdersResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetOrders")
+                .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Gets a list of orders")
+                .WithDescription("Retrieves a paginated list of orders with optional filtering.");
+        }
     }
 }
