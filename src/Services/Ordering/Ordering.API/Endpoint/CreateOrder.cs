1f8b71ed5e3a78c9adfcf20760fe30f8ae236e82
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2026-01-05 17:00:44 +0600

    post order api created

--- src/Services/Ordering/Ordering.API/Endpoint/CreateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoint/CreateOrder.cs
@@ -1,10 +1,31 @@
-﻿namespace Ordering.API.Endpoint
+﻿
+using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.API.Endpoint
 {
     // Accepts a request to create a new order
     // Maps the request to create order command
     // uses the mediator to send the command to the appropriate handler
     // returns a response with the created order id
-    public class CreateOrder
+
+    public record CreateOrderRequest(OrderDto Order);
+    public record CreateOrderResponse(Guid Id);
+    public class CreateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateOrderCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateOrderResponse>();
+                return Results.Created($"/orders/{response.Id}", response);
+            })
+                .WithName("CreateOrder")
+                .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Creates a new order")
+                .WithDescription("Creates a new order with the provided details.");
+        }
     }
 }
---
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
+
+namespace Ordering.API
+{
+    public class GlobalUsing
+    {
+    }
+}
