774f5d75e62265314a6158f6db1c467db910186e
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2026-01-08 18:13:49 +0600

    add user friendly json response for health check api

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,15 +1,21 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             // Add API services registrations here
 
             services.AddCarter();
             services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
+
+
             return services;
         }
 
@@ -19,6 +25,10 @@ namespace Ordering.API
             app.MapCarter();
             app.UseExceptionHandler(options =>
             {
+            });
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
             });
             return app;
         }
