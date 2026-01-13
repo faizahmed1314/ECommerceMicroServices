0f5aee2c82f3d2b4c81db536138203464db6fd15
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2026-01-13 18:44:56 +0600

    created integration and basketcheckout event

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace BuildingBlocks.Messaging.Events
+{
+    public record BasketCheckoutEvent : IntegrationEvent
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public Guid CustomerId { get; set; }
+
+        // Shipping Address & billing address
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string AddressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        // Payment
+        public string CardNumber { get; set; }
+        public string CardName { get; set; }
+        public DateTime Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
