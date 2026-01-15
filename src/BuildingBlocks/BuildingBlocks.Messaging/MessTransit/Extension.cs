14157a47379876ff50b299f2540f87f48e5754fc
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2026-01-15 18:45:16 +0600

    configure message broker

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MessTransit/Extension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MessTransit
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddMessTransitMessaging(this IServiceCollection services, Assembly? assembly = null)
+        {
+            // Configure MassTransit with RabbitMQ
+
+            return services;
+        }
+    }
+}
