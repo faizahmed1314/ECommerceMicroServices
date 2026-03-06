2bd205804465fd123efe061655bc94ecbd3ffd28
Faiz Ahmed fj20 <foyezahmedfj20@gmail.com>
2026-03-06 11:07:55 +0600

    update the method name

--- src/BuildingBlocks/BuildingBlocks.Messaging/MessTransit/Extension.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MessTransit/Extension.cs
@@ -1,14 +1,38 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace BuildingBlocks.Messaging.MessTransit
 {
     public static class Extension
     {
-        public static IServiceCollection AddMessTransitMessaging(this IServiceCollection services, Assembly? assembly = null)
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
         {
             // Configure MassTransit with RabbitMQ
 
+            services.AddMassTransit(config =>
+            {
+                config.SetKebabCaseEndpointNameFormatter();
+
+                // Register all consumers from the specified assembly
+                if (assembly != null)
+                {
+                    config.AddConsumers(assembly);
+                }
+
+                config.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(new Uri(configuration["MessageBroker: Host"]!), h =>
+                    {
+                        // You can configure username and password here if needed
+                        h.Username(configuration["MessageBroker:UserName"]);
+                        h.Password(configuration["MessageBroker:Password"]);
+                    });
+                    // Configure endpoints for all registered consumers
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
             return services;
         }
     }
